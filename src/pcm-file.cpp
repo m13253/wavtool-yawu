ba5e28a05b310e448039470252d16844de04e194
Star Brilliant <m13253@hotmail.com>
2015-07-24 17:34:44 +0800

    PCMFile

---
+++ src/pcm-file.cpp
@@ -0,0 +1,150 @@
+/*
+    wavtool-yawu
+    Copyright (C) 2015 StarBrilliant <m13253@hotmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3.0 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not,
+    see <http://www.gnu.org/licenses/>.
+*/
+
+#include "pcm-file.hpp"
+#include <cstdio>
+#include <ios>
+#include <libwintf8/fileio.h>
+#include <libwintf8/u8str.h>
+#ifdef _WIN32
+#include <windows.h>
+#define ENABLE_SNDFILE_WINDOWS_PROTOTYPES 1
+#endif
+#include <sndfile.hh>
+
+namespace RUCE {
+
+struct PCMFile::Private {
+    SndfileHandle sndfile;
+};
+
+PCMFile::PCMFile() {
+}
+
+PCMFile::PCMFile(const WTF8::u8string &filename, std::ios_base::openmode mode, int format, int channels, int sample_rate) {
+    open(filename, mode, format, channels, sample_rate);
+}
+
+PCMFile::~PCMFile() {
+    close();
+}
+
+PCMFile &PCMFile::open(const WTF8::u8string &filename, std::ios_base::openmode mode, int format, int channels, int sample_rate) {
+    close();
+    int sfmode = SFM_READ;
+    if((mode & (std::ios_base::in | std::ios_base::out)) == (std::ios_base::in | std::ios_base::out)) {
+        sfmode = SFM_RDWR;
+    } else if(mode & std::ios_base::out) {
+        sfmode = SFM_WRITE;
+    }
+#ifdef _WIN32
+    p->sndfile = SndFileHandle(filename.to_wide().c_str(), sfmode, format, channels, sample_rate);
+#else
+    p->sndfile = SndFileHandle(filename, sfmode, format, channels, sample_rate);
+#endif
+    if(!p->sndfile)
+        throw FileError(p->sndfile.strError());
+    return *this;
+}
+
+PCMFile &PCMFile::close() {
+    p->sndfile = SndFileHandle();
+    return *this;
+}
+
+int64_t PCMFile::frames() const {
+    return p->sndfile.frames();
+}
+
+int PCMFile::format() const {
+    return p->sndfile.format();
+}
+
+int PCMFile::channels() const {
+    return p->sndfile.channels();
+}
+
+int PCMFile::sample_rate() const {
+    return p->sndfile.samplerate();
+}
+
+size_t PCMFile::read(short *output_buf, size_t frames) {
+    return p->sndfile.readf(output_buf, frames);
+}
+
+size_t PCMFile::read(int *output_buf, size_t frames) {
+    return p->sndfile.readf(output_buf, frames);
+}
+
+size_t PCMFile::read(float *output_buf, size_t frames) {
+    return p->sndfile.readf(output_buf, frames);
+}
+
+size_t PCMFile::read(double *output_buf, size_t frames) {
+    return p->sndfile.readf(output_buf, frames);
+}
+
+size_t PCMFile::write(const short *input_buf, size_t frames) {
+    auto write_count = p->sndfile.writef(input_buf, frames);
+    if(write_count == 0)
+        throw FileError(p->sndfile.strError());
+    return write_count;
+}
+
+size_t PCMFile::write(const int *input_buf, size_t frames) {
+    auto write_count = p->sndfile.writef(input_buf, frames);
+    if(write_count == 0)
+        throw FileError(p->sndfile.strError());
+    return write_count;
+}
+
+size_t PCMFile::write(const float *input_buf, size_t frames) {
+    auto write_count = p->sndfile.writef(input_buf, frames);
+    if(write_count == 0)
+        throw FileError(p->sndfile.strError());
+    return write_count;
+}
+
+size_t PCMFile::write(const double *input_buf, size_t frames) {
+    auto write_count = p->sndfile.writef(input_buf, frames);
+    if(write_count == 0)
+        throw FileError(p->sndfile.strError());
+    return write_count;
+}
+
+int64_t PCMFile::seek(int64_t frames, int whence) {
+    auto new_offset = p->sndfile.seek(frames, whence);
+    if(new_offset < 0)
+        throw FileError(p->sndfile.strError());
+    return new_offset;
+}
+
+int PCMFile::command(int cmd, void *data, size_t data_size) {
+    return p->sndfile.command(cmd, data, data_size);
+}
+
+SndfileHandle &PCMFile::sndfile_cxx() const {
+    return p->sndfile;
+}
+
+SNDFILE *PCMFile::sndfile_c() const {
+    return p->sndfile.rawHandle();
+}
+
+}
